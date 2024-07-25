4b4c3f89f27d8eddccf5ce292ba56a5c2ba4a07d
Shanth05 <79297844+Shanth05@users.noreply.github.com>
2024-07-25 14:25:48 +0530

    FEAT : Combo Box

--- Fsmb 02/src/MainformController.java
+++ Fsmb 02/src/MainformController.java
@@ -1,2 +1,18 @@
+import javafx.scene.control.TextField;
+
+import java.util.Observable;
+
 public class MainformController {
+
+    public TextField txtEmail;
+    public TextField txtName;
+
+    public void initialize() {
+        txtEmail.textProperty().addListener((Observable, oldValue, newValue) -> {
+                    System.out.println("new value : " + newValue);
+                    System.out.println("old value : " + oldValue);
+                    txtName.setText(newValue);
+                }
+        );
+    }
 }
